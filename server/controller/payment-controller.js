84deb71229fbcc52536adbfe527ed16b8c36815a
Sushant <sushant889427@gmail.com>
2024-12-03 18:05:38 +0530

    Debugging

--- server/controller/payment-controller.js
+++ server/controller/payment-controller.js
@@ -3,79 +3,70 @@ import axios from 'axios';
 import dotenv from 'dotenv'
 
 dotenv.config();
-
-// real
-// const CLIENT_URL = process.env.CLIENT_URL;
-// const SERVER_URL = process.env.SERVER_URL;
-// const SALT_KEY = process.env.SALT_KEY;
-// const MERCHANT_ID = process.env.MERCHANT_ID;
-
-// fake
 const CLIENT_URL = process.env.CLIENT_URL;
 const SERVER_URL = process.env.SERVER_URL;
-const PHONE_PE_HOST_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox";
-const SALT_KEY = '099eb0cd-02cf-4e2a-8aca-3e6c6aff0399';
-const SALT_INDEX = 1;
-const MERCHANT_ID = 'PGTESTPAYUAT';
-const APP_BE_URL = process.env.SERVER_URL;
+const SALT_KEY = process.env.SALT_KEY;
+const MERCHANT_ID = process.env.MERCHANT_ID;
 
-export const doPayment = async (req, res) => {
+export const doPayment = async (request, response) => {
     // console.log("============ >inside doPayment route and salt,merchant,server_url and client_url = ", CLIENT_URL, SERVER_URL, SALT_KEY, MERCHANT_ID)
-    const amount = +req.query.amount;
+    try {
+        const transactionId = request.body.transactionId;
 
-    // User ID is the ID of the user present in our application DB
-    let userId = "MUID123";
-
-    // Generate a unique merchant transaction ID for each transaction
-    let merchantTransactionId = req.body.transactionId;
-
-    let normalPayLoad = {
-        merchantId: MERCHANT_ID,
-        merchantTransactionId: merchantTransactionId,
-        merchantUserId: userId,
-        amount: amount * 100, // converting to paise
-        redirectUrl: `${APP_BE_URL}/status/${merchantTransactionId}`,
-        redirectMode: "REDIRECT",
-        mobileNumber: "9999999999",
-        paymentInstrument: {
-            type: "PAY_PAGE",
-        },
-    };
-
-    // Make a base64-encoded payload
-    let bufferObj = Buffer.from(JSON.stringify(normalPayLoad), "utf8");
-    let base64EncodedPayload = bufferObj.toString("base64");
-
-    // X-VERIFY => SHA256(base64EncodedPayload + "/pg/v1/pay" + SALT_KEY) + ### + SALT_INDEX
-    let string = base64EncodedPayload + "/pg/v1/pay" + SALT_KEY;
-    let sha256_val = crypto.createHash('sha256').update(string).digest('hex')
-    let xVerifyChecksum = sha256_val + "###" + SALT_INDEX;
-
-    console.log("=============> before")
-
-    axios.post(
-        `${PHONE_PE_HOST_URL}/pg/v1/pay`,
-        { request: base64EncodedPayload },
-        {
-            headers: {
-                "Content-Type": "application/json",
-                "X-VERIFY": xVerifyChecksum,
-                accept: "application/json",
-            },
+        const data = {
+            merchantId: MERCHANT_ID,
+            merchantTransactionId: transactionId,
+            name: request.body.name,
+            amount: request.body.amount * 100,      // in paise
+            redirectUrl: `${SERVER_URL}/status/${transactionId}`,
+            redirectMode: "POST",
+            mobileNumber: request.body.number,
+            paymentInstrument: {
+                type: "PAY_PAGE"
+            }
         }
-    )
-        .then(function (response) {
-            console.log("response ======== ->", response.data);
-            res.redirect(response.data.data.instrumentResponse.redirectInfo.url);
+
+        // console.log("========================> server rediret url = ", `${SERVER_URL}/status/${transactionId}`)
+
+        const payload = JSON.stringify(data)
+        const payloadMain = Buffer.from(payload).toString('base64')
+        const keyIndex = 1
+        const string = payloadMain + '/pg/v1/pay' + SALT_KEY
+        const sha256 = crypto.createHash('sha256').update(string).digest('hex')
+        const checksum = sha256 + '###' + keyIndex
+
+        const testURL = 'https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay'
+
+        const options = {
+            method: "POST",
+            url: testURL,
+            headers: {
+                accept: 'application/json',
+                'Content-Type': 'application/json',
+                'X-VERIFY': checksum
+            },
+            data: {
+                request: payloadMain
+            }
+        }
+
+        await axios(options).then(function (res) {
+            // console.log("=============> response after .then = ", res)
+            return response.json(res.data)
+        }).catch(function (error) {
+            // console.log("===================> error in payment-controller do-payment 1 = ", error.message)
+            return response.status(500).json({ message: `Payment failed ! ${error.message}` })
         })
-        .catch(function (error) {
-            console.log("==============> error in 1st = ", error.message)
-            res.send(error);
-        });
+    }
+    catch (error) {
+        console.log("===================> error in payment-controller do-payment 2 = ", error.message)
+        response.status(500).json({ message: error.message })
+    }
 }
 
+
 export const status = async (request, response) => {
-    console.log("============ >inside status route")
+    // console.log("============ >inside status route")
     const merchantTransactionId = request.params.txnId;
     const merchantId = MERCHANT_ID;
     const keyIndex = 1
@@ -97,7 +88,7 @@ export const status = async (request, response) => {
     }
 
     axios.request(options).then(function (res) {
-        console.log("================> inside axios.request after .then()")
+        // console.log("================> inside axios.request after .then()")
         if (res.data.success === true) {
             const url = `${CLIENT_URL}/success`
             response.redirect(url)
@@ -107,7 +98,7 @@ export const status = async (request, response) => {
             response.redirect(url)
         }
     }).catch(function (error) {
-        console.log("===================> error in payment-controller status = ", error.message)
+        // console.log("===================> error in payment-controller status = ", error.message)
         response.status(500).json({ message: error.message })
     })
 }
--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const Connection = async (USERNAME, PASSWORD) => {
     const ENCODED_PASSWORD = encodeURIComponent(PASSWORD);
-    console.log("========================> username and password for database = ", USERNAME, " and ", PASSWORD)
+    // console.log("========================> username and password for database = ", USERNAME, " and ", PASSWORD)
     const URL = `mongodb+srv://${USERNAME}:${ENCODED_PASSWORD}@cluster0.puwub.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true })
