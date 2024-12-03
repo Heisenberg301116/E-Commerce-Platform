4e159cdea46a959869809c5aadb7424e4f72d456
Sushant <sushant889427@gmail.com>
2024-12-03 16:53:42 +0530

    Debugging

--- server/controller/payment-controller.js
+++ server/controller/payment-controller.js
@@ -13,67 +13,63 @@ dotenv.config();
 // fake
 const CLIENT_URL = process.env.CLIENT_URL;
 const SERVER_URL = process.env.SERVER_URL;
+const PHONE_PE_HOST_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox";
 const SALT_KEY = '099eb0cd-02cf-4e2a-8aca-3e6c6aff0399';
+const SALT_INDEX = 1;
 const MERCHANT_ID = 'PGTESTPAYUAT';
+const APP_BE_URL = process.env.SERVER_URL;
 
-export const doPayment = async (request, response) => {
+export const doPayment = async (req, res) => {
     // console.log("============ >inside doPayment route and salt,merchant,server_url and client_url = ", CLIENT_URL, SERVER_URL, SALT_KEY, MERCHANT_ID)
-    try {
-        const transactionId = request.body.transactionId;
+    const amount = +req.query.amount;
 
-        const data = {
-            merchantId: MERCHANT_ID,
-            merchantTransactionId: transactionId,
-            name: request.body.name,
-            amount: request.body.amount * 100,      // in paise
-            redirectUrl: `${SERVER_URL}/status/${transactionId}`,
-            redirectMode: "POST",
-            mobileNumber: request.body.number,
-            paymentInstrument: {
-                type: "PAY_PAGE"
-            }
-        }
+    // User ID is the ID of the user present in our application DB
+    let userId = "MUID123";
 
-        // console.log("========================> server rediret url = ", `${SERVER_URL}/status/${transactionId}`)
+    // Generate a unique merchant transaction ID for each transaction
+    let merchantTransactionId = uniqid();
 
-        const payload = JSON.stringify(data)
-        const payloadMain = Buffer.from(payload).toString('base64')
-        const keyIndex = 1
-        const string = payloadMain + '/pg/v1/pay' + SALT_KEY
-        const sha256 = crypto.createHash('sha256').update(string).digest('hex')
-        const checksum = sha256 + '###' + keyIndex
+    let normalPayLoad = {
+        merchantId: MERCHANT_ID,
+        merchantTransactionId: merchantTransactionId,
+        merchantUserId: userId,
+        amount: amount * 100, // converting to paise
+        redirectUrl: `${APP_BE_URL}/status/${merchantTransactionId}`,
+        redirectMode: "REDIRECT",
+        mobileNumber: "9999999999",
+        paymentInstrument: {
+            type: "PAY_PAGE",
+        },
+    };
 
-        // const testURL = 'https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay'      // real
+    // Make a base64-encoded payload
+    let bufferObj = Buffer.from(JSON.stringify(normalPayLoad), "utf8");
+    let base64EncodedPayload = bufferObj.toString("base64");
 
-        const testURL = 'https://api-preprod.phonepe.com/apis/pg-sandbox'     // fake
+    // X-VERIFY => SHA256(base64EncodedPayload + "/pg/v1/pay" + SALT_KEY) + ### + SALT_INDEX
+    let string = base64EncodedPayload + "/pg/v1/pay" + SALT_KEY;
+    let sha256_val = sha256(string);
+    let xVerifyChecksum = sha256_val + "###" + SALT_INDEX;
 
-        const options = {
-            method: "POST",
-            url: testURL,
+    axios.post(
+        `${PHONE_PE_HOST_URL}/pg/v1/pay`,
+        { request: base64EncodedPayload },
+        {
             headers: {
-                accept: 'application/json',
-                'Content-Type': 'application/json',
-                'X-VERIFY': checksum
+                "Content-Type": "application/json",
+                "X-VERIFY": xVerifyChecksum,
+                accept: "application/json",
             },
-            data: {
-                request: payloadMain
-            }
         }
-
-        await axios(options).then(function (res) {
-            console.log("=============> response after .then = ", res)
-            return response.json(res.data)
-        }).catch(function (error) {
-            console.log("===================> error in payment-controller do-payment 1 = ", error.message)
-            return response.status(500).json({ message: `Payment failed ! ${error.message}` })
+    )
+        .then(function (response) {
+            console.log("response->", response.data);
+            res.redirect(response.data.data.instrumentResponse.redirectInfo.url);
         })
-    }
-    catch (error) {
-        console.log("===================> error in payment-controller do-payment 2 = ", error.message)
-        response.status(500).json({ message: error.message })
-    }
+        .catch(function (error) {
+            res.send(error);
+        });
 }
-
 
 export const status = async (request, response) => {
     console.log("============ >inside status route")
